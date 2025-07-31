50e78440fe1a351aae6c3eee20e991d1da685a44
Alex <axpg31@gmail.com>
2025-07-31 16:02:21 +0200

    #

--- middleware.ts
+++ middleware.ts
@@ -41,9 +41,3 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
-  return NextResponse.next();
-}
-
-export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-};

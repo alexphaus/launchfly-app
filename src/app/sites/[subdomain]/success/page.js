a68defa96be7fff06893cbabadc85a4143df0ad4
Alex <axpg31@gmail.com>
2025-08-01 10:53:37 +0200

    #

--- src/app/sites/[subdomain]/success/page.js
+++ src/app/sites/[subdomain]/success/page.js
@@ -72,7 +72,7 @@ export default function PurchaseSuccess({ params }) {
         {/* Action Buttons */}
         <div className="space-y-3">
           <Link
-            href={`/${params.subdomain}`}
+            href={`/sites/${params.subdomain}`}
             className="w-full bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors font-medium inline-block"
           >
             Return to Website
